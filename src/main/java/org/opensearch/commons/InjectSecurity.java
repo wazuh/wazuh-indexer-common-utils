fbf5f6104554f61a5f63f8d689b77dd7702c8862
QU3B1M <kevin.ledesma@wazuh.com>
2026-06-11 08:23:14 -0300

    Another test commit

--- src/main/java/org/opensearch/commons/InjectSecurity.java
+++ src/main/java/org/opensearch/commons/InjectSecurity.java
@@ -17,6 +17,7 @@ import java.util.StringJoiner;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.commons.authuser.User;
